b3d199cfb61f6a0b5627ef44bc53f7f06e48e6a4
White-Devil347 <asurx64@gmail.com>
2026-09-07 02:32:39 +0530

    ci: Fix base64 decode by ignoring garbage characters, bump to 0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.quickdrop.sharetarget"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.6.2"
+        versionCode = 6
+        versionName = "0.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

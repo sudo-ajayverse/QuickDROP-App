8cb84bad715573f29c1fa708b753a507c993014b
White-Devil347 <asurx64@gmail.com>
2026-09-07 01:51:33 +0530

    chore: Bump version to 0.5.0 for auto-update test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.quickdrop.sharetarget"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

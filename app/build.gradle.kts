032f767ea7672c353e3e8f623cd98c1f7a4b65b2
White-Devil347 <asurx64@gmail.com>
2026-09-07 02:15:23 +0530

    chore: Remove duplicate android.yml workflow, bump to 0.6.1, ensure QuickDROP.apk in release.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.quickdrop.sharetarget"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.5.1"
+        versionCode = 4
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
